1c2ef22320b372f91f70bc8e719dd3b0f8554818
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-10-16 11:52:53 +0200

    op

---
+++ Code/Programmering/3.semester/Spring/Frontend/Javascript/nemjavascript/src/main/java/com/example/nemjavascript/NemjavascriptApplication.java
@@ -0,0 +1,13 @@
+package com.example.nemjavascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemjavascriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemjavascriptApplication.class, args);
+	}
+
+}
---
+++ Code/Programmering/3.semester/Spring/Frontend/Javascript/nemjavascript/src/test/java/com/example/nemjavascript/NemjavascriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nemjavascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemjavascriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
